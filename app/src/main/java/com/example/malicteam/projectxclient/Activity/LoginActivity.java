0ac375d1792ec489631fda83b184f00740ce95ab
maayanbraks <maayanbraksmayer@gmail.com>
2018-05-11 14:27:52 +0300

    Changed to fragments: -Account Details -Main Screen (Event List) -FriendsList -friend details -Event Details -Reset Password - (there is also an activity, for scenario like click on reset password from login screen)

--- app/src/main/java/com/example/malicteam/projectxclient/Activity/LoginActivity.java
+++ app/src/main/java/com/example/malicteam/projectxclient/Activity/LoginActivity.java
@@ -3,10 +3,8 @@ package com.example.malicteam.projectxclient.Activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatDelegate;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,14 +13,14 @@ import android.widget.Toast;
 
 import com.example.malicteam.projectxclient.Common.Consts;
 
+import com.example.malicteam.projectxclient.Common.MyApp;
 import com.example.malicteam.projectxclient.Model.Repository;
 import com.example.malicteam.projectxclient.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import com.example.malicteam.projectxclient.Model.User;
+
+import Requests.LoginRequestData;
 
 
 public class LoginActivity extends Activity {
@@ -40,9 +38,7 @@ public class LoginActivity extends Activity {
         inputEmail = (EditText) findViewById(R.id.email);
         inputPassword = (EditText) findViewById(R.id.password);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-        btnSignup = (Button) findViewById(R.id.btn_signup);
-        btnLogin = (Button) findViewById(R.id.btn_login);
-        btnReset = (Button) findViewById(R.id.btn_reset_password);
+
 
         //Get Firebase auth instance
         FirebaseAuth auth = FirebaseAuth.getInstance();
@@ -55,8 +51,13 @@ public class LoginActivity extends Activity {
             finish();
         }
 
+        initButtons();
+    }
 
-
+    private void initButtons() {
+        btnSignup = (Button) findViewById(R.id.btn_signup);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnReset = (Button) findViewById(R.id.btn_reset_password);
         btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,83 +79,74 @@ public class LoginActivity extends Activity {
         });
     }
 
-    private void tryLogin(){
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-
+    private void tryLogin() {
         //get Inputs
         String email = inputEmail.getText().toString();
         final String password = inputPassword.getText().toString();
         //check the inputs
         if (TextUtils.isEmpty(email)) {
-            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MyApp.getContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
             return;
         }
         if (TextUtils.isEmpty(password)) {
-            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            Toast.makeText(MyApp.getContext(), "Enter password!", Toast.LENGTH_SHORT).show();
             return;
         }
 
         progressBar.setVisibility(View.VISIBLE);
 
-
-        //CLOUDMANAGER ////////////////////////////////////////////
-        Repository.instance.logIn(email,password, new Repository.RepositoryCallback() {
+        //CLOUDMANAGER //
+        Repository.instance.logIn(email, password, new Repository.RepositoryCallback() {
             @Override
             public void onComplete(Object data) {
-                String respone=data.toString();
-                switch (respone)
-                {
+                String response = data.toString();
+                switch (response) {
                     case "TechnicalError":
-                        Toast.makeText(getApplicationContext(), "Technical error,please try again.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MyApp.getContext(), "Technical error,please try again.", Toast.LENGTH_SHORT).show();
                         break;
                     case "UserIsNotExist":
-                        Toast.makeText(getApplicationContext(), "Can`t find username.", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MyApp.getContext(), "Can`t find username.", Toast.LENGTH_SHORT).show();
                         break;
                     case "True":
-                        Toast.makeText(getApplicationContext(), "logging in", Toast.LENGTH_SHORT).show();
+                        //LoginRequestData lrd = new LoginRequestData();
+                        Toast.makeText(MyApp.getContext(), "logging in", Toast.LENGTH_SHORT).show();
                         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                         startActivity(intent);
                         finish();
                         break;
                     case "False":
-                        Toast.makeText(getApplicationContext(), getString(R.string.auth_failed), Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MyApp.getContext(), getString(R.string.auth_failed), Toast.LENGTH_SHORT).show();
                         break;
                 }
-
             }
-
             @Override
             public void onCancel() {
 
             }
 
         });
-
-
-            ////////////////////////////
+        ////////////////////////////
         //authenticate user
-        auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        progressBar.setVisibility(View.GONE);
-                        if (!task.isSuccessful()) {
-                            if (password.length() < 6) {
-                                inputPassword.setError(getString(R.string.minimum_password));
-                            } else {
-                                Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
-                            }
-                        } else {
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
-                            int id = User.generateId(FirebaseAuth.getInstance().getCurrentUser().getEmail());
-                            intent.putExtra(Consts.USER_ID, id);
-                            startActivity(intent);
-                            finish();
-                        }
-                    }
-                });
-
-
+//        auth.signInWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        progressBar.setVisibility(View.GONE);
+//                        if (!task.isSuccessful()) {
+//                            if (password.length() < 6) {
+//                                inputPassword.setError(getString(R.string.minimum_password));
+//                            } else {
+//                                Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+//                            }
+//                        } else {
+//                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+//                            int id = User.generateId(FirebaseAuth.getInstance().getCurrentUser().getEmail());
+//                            intent.putExtra(Consts.USER_ID, id);
+//                            startActivity(intent);
+//                            finish();
+//                        }
+//                    }
+//                });
     }
 }
 
