9a1876488a590ca6b825d2bcbf35ae7079b8af01
Charcon <sharkonz@gmail.com>
2018-06-01 16:43:24 +0300

    Fixed: Recording activity: on User Join\Left

--- app/src/main/java/com/example/malicteam/projectxclient/Activity/RecordingActivity.java
+++ app/src/main/java/com/example/malicteam/projectxclient/Activity/RecordingActivity.java
@@ -156,6 +156,10 @@ public class RecordingActivity extends AppCompatActivity {
                             stopPlaying();
                         if (recorder.isRecording())
                             startRecordOrSaveIt();
+                            else {
+                            shareEvent();
+                            finish();
+                        }
                         if (!(checkMeAdmin())) {
                             leaveEvent();
                         }
